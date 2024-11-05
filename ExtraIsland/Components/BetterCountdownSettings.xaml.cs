31dc60c8fa06dc929e38b7f7acf481b5f57d7ff0
LiPolymer <fish@lipoly.ink>
2024-11-05 23:34:43 +0800

    feat: Setting page of BetterCountdown now truly usable

--- ExtraIsland/Components/BetterCountdownSettings.xaml.cs
+++ ExtraIsland/Components/BetterCountdownSettings.xaml.cs
@@ -9,31 +9,4 @@ public partial class BetterCountdownSettings : ComponentBase<BetterCountdownConf
     public BetterCountdownSettings() {
         InitializeComponent();
     }
-
-    void UpdateConfig() {
-        Settings.TargetDate = (SetCalendar.SelectedDate!.Value.Date).ToString("s");
-        Settings.Prefix = TBox.Text;
-        Settings.Suffix = SBox.Text;
-    }
-
-    void LoadConfig() {
-        Settings.TargetDate ??= DateTime.Now.ToString("s");
-        Settings.Prefix ??= "现在";
-        Settings.Suffix ??= "过去了";
-        SetCalendar.SelectedDate = Convert.ToDateTime(Settings.TargetDate);
-        TBox.Text = Settings.Prefix;
-        SBox.Text = Settings.Suffix;
-    }
-    
-    void Calendar_OnSelectedDatesChanged(object? sender, SelectionChangedEventArgs e) {
-        UpdateConfig();
-    }
-
-    void BetterCountdownSettings_OnLoaded(object sender, RoutedEventArgs e) {
-        LoadConfig();
-    }
-
-    void TBox_OnTextChanged(object sender,TextChangedEventArgs e) {
-        UpdateConfig();
-    }
 }
