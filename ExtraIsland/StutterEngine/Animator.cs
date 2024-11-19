d876d9b2780ba5c8b51bdb6b7df707a418e4ec3d
LiPolymer <fish@lipoly.ink>
2024-11-19 23:05:34 +0800

    frame: Develop StutterEngine to improve animation perf while stable and easy.

---
+++ ExtraIsland/StutterEngine/Animator.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System;
+using System.Timers;
+using System.Windows;
+using System.Windows.Media;
+using Timer = System.Threading.Timer;
+
+namespace ExtraIsland.StutterEngine;
+
+public class Animator {
+
+    Animator(TimeSpan interval) {
+        _ticker = new System.Timers.Timer(interval);
+        _interval = interval;
+    }
+
+    readonly TimeSpan _interval;
+    readonly System.Timers.Timer _ticker;
+    
+    public void StartTranslateTransform(TranslateTransform translate, TranslateTransformModifier modifier, TimeSpan duration) {
+        _ticker.AutoReset = true;
+        int tick = 0;
+        _ticker.Elapsed += (o,e) => {
+            modifier(translate, tick, _interval, duration);
+            if (tick * _interval.Milliseconds >= duration.Milliseconds) {
+                _ticker.Stop();
+            }
+            tick ++;
+        };
+        _ticker.Start();
+    }
+    
+    public delegate void TranslateTransformModifier(TranslateTransform translate, int tick , TimeSpan interval,TimeSpan duration);
+}
