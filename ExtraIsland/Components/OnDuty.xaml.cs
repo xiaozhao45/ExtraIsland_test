2106da626b649ab763997b489edd153cffb05099
LiPolymer <fish@lipoly.ink>
2024-12-22 14:18:37 +0800

    feat: add component OnDuty

---
+++ ExtraIsland/Components/OnDuty.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Controls;
+using ClassIsland.Core.Abstractions.Controls;
+
+namespace ExtraIsland.Components;
+
+public partial class OnDuty : ComponentBase {
+    public OnDuty() {
+        InitializeComponent();
+    }
+}
