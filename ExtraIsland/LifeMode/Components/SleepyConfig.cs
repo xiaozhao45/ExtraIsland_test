95743ba68cb0dac49b5bf5da17a35b5053221992
LiPolymer <fish@lipoly.ink>
2025-01-27 23:04:55 +0800

    feat: set default Sleepy API to ClassIsland developers

--- ExtraIsland/LifeMode/Components/SleepyConfig.cs
+++ ExtraIsland/LifeMode/Components/SleepyConfig.cs
@@ -1,7 +1,7 @@
 ﻿namespace ExtraIsland.LifeMode.Components;
 
 public class SleepyConfig {
-    public string ApiUrl { get; set; } = "";
+    public string ApiUrl { get; set; } = "https://sleepy.developers.classisland.tech/query";
     public TimeSpan UpdateInterval { get; set; } = TimeSpan.Zero;
 
     public int UpdateIntervalSeconds {
