de71ddb6dd02f7be09c1db61b46dc407c633089c
LiPolymer <fish@lipoly.ink>
2025-02-02 23:06:53 +0800

    chore: restore code style !noCi

--- ExtraIsland/ConfigHandlers/MainConfigHandler.cs
+++ ExtraIsland/ConfigHandlers/MainConfigHandler.cs
@@ -6,18 +6,18 @@ using CommunityToolkit.Mvvm.ComponentModel;
 namespace ExtraIsland.ConfigHandlers;
 
 public class MainConfigHandler {
-    private readonly string _configPath;
+    readonly string _configPath;
     public MainConfigData Data { get; set; }
-    
+
     public MainConfigHandler() {
-        _configPath = Path.Combine(GlobalConstants.PluginConfigFolder!, "Main.json");
+        _configPath = Path.Combine(GlobalConstants.PluginConfigFolder!,"Main.json");
         Data = new MainConfigData();
-        
+
         InitializeConfig();
         SubscribeToChanges();
     }
 
-    private void InitializeConfig() {
+    void InitializeConfig() {
         if (string.IsNullOrEmpty(GlobalConstants.PluginConfigFolder)) {
             throw new InvalidOperationException("配置文件夹路径未设置");
         }
@@ -37,36 +37,31 @@ public class MainConfigHandler {
         }
     }
 
-    private void SubscribeToChanges() {
+    void SubscribeToChanges() {
         Data.PropertyChanged += OnPropertyChanged;
         Data.TinyFeatures.JuniorGuide.PropertyChanged += OnPropertyChanged;
     }
 
-    private void OnPropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e) {
+    void OnPropertyChanged(object? sender,System.ComponentModel.PropertyChangedEventArgs e) {
         Save();
     }
 
-    private void Save() {
-        try
-        {
-            ConfigureFileHelper.SaveConfig(_configPath, Data);
+    void Save() {
+        try {
+            ConfigureFileHelper.SaveConfig(_configPath,Data);
         }
-        catch (Exception ex)
-        {
+        catch (Exception ex) {
             Console.WriteLine($"保存配置文件失败: {ex.Message}");
             throw;
         }
     }
 }
 
-public partial class MainConfigData : ObservableObject 
-{
-    private bool _isLifeModeActivated;
-    public bool IsLifeModeActivated
-    {
+public partial class MainConfigData : ObservableObject {
+    bool _isLifeModeActivated;
+    public bool IsLifeModeActivated {
         get => _isLifeModeActivated;
-        set
-        {
+        set {
             if (value == _isLifeModeActivated) return;
             _isLifeModeActivated = value;
             OnPropertyChanged();
@@ -75,70 +70,58 @@ public partial class MainConfigData : ObservableObject
 
     public TinyFeaturesConfig TinyFeatures { get; set; } = new TinyFeaturesConfig();
 
-    public partial class TinyFeaturesConfig : ObservableObject
-    {
+    public partial class TinyFeaturesConfig : ObservableObject {
         public JuniorGuideConfig JuniorGuide { get; set; } = new JuniorGuideConfig();
 
-        public partial class JuniorGuideConfig : ObservableObject
-        {
-            private bool _enabled;
-            public bool Enabled
-            {
+        public class JuniorGuideConfig : ObservableObject {
+            bool _enabled;
+            public bool Enabled {
                 get => _enabled;
-                set
-                {
+                set {
                     if (value == _enabled) return;
                     _enabled = value;
                     OnPropertyChanged();
                 }
             }
 
-            private string _header = "欢迎 · 电子白板使用导引";
-            public string Header
-            {
+            string _header = "欢迎 · 电子白板使用导引";
+            public string Header {
                 get => _header;
-                set
-                {
+                set {
                     if (value == _header) return;
                     _header = value;
                     OnPropertyChanged();
                 }
             }
 
-            private int _keepTime = 60000;
-            public int KeepTime
-            {
+            int _keepTime = 60000;
+            public int KeepTime {
                 get => _keepTime;
-                set
-                {
+                set {
                     if (value == _keepTime) return;
                     _keepTime = value;
                     OnPropertyChanged();
                 }
             }
 
-            private string _content = "# 一体机交接指南\r\n"
-                                  + "这是一个示列\r\n"
-                                  + "*支持Markdown*\r\n"
-                                  + "> 请按需修改\r\n"
-                                  + "> awa!";
-            public string Content
-            {
+            string _content = "# 一体机交接指南\r\n"
+                              + "这是一个示列\r\n"
+                              + "*支持Markdown*\r\n"
+                              + "> 请按需修改\r\n"
+                              + "> awa!";
+            public string Content {
                 get => _content;
-                set
-                {
+                set {
                     if (value == _content) return;
                     _content = value;
                     OnPropertyChanged();
                 }
             }
 
-            private string _link = "https://docs.classisland.tech/";
-            public string Link
-            {
+            string _link = "https://docs.classisland.tech/";
+            public string Link {
                 get => _link;
-                set
-                {
+                set {
                     if (value == _link) return;
                     _link = value;
                     OnPropertyChanged();
--- ExtraIsland/SettingsPages/MainSettingsPage.xaml.cs
+++ ExtraIsland/SettingsPages/MainSettingsPage.xaml.cs
@@ -6,20 +6,16 @@ using System.ComponentModel;
 
 namespace ExtraIsland.SettingsPages;
 
-[SettingsPageInfo("extraisland.master","ExtraIsland·主设置",PackIconKind.BoxAddOutline, PackIconKind.BoxAdd)]
-public partial class MainSettingsPage
-{
-    public MainSettingsPage()
-    {
+[SettingsPageInfo("extraisland.master","ExtraIsland·主设置",PackIconKind.BoxAddOutline,PackIconKind.BoxAdd)]
+public partial class MainSettingsPage {
+    public MainSettingsPage() {
         Settings = GlobalConstants.Handlers.MainConfig!.Data;
         InitializeComponent();
         Settings.PropertyChanged += SettingsOnPropertyChanged;
     }
     public MainConfigData Settings { get; set; }
-    private void SettingsOnPropertyChanged(object? sender, PropertyChangedEventArgs e)
-    {
-        if (e.PropertyName == nameof(Settings.IsLifeModeActivated))
-        {
+    void SettingsOnPropertyChanged(object? sender,PropertyChangedEventArgs e) {
+        if (e.PropertyName == nameof(Settings.IsLifeModeActivated)) {
             RequestRestart();
         }
     }
