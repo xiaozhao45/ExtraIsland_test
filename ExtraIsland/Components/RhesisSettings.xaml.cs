780c93979f3f298c1d9854a568445ab48e44d1b3
XiaoZhao45 <130202157+xiaozhao45@users.noreply.github.com>
2025-03-02 16:24:47 +0800

    Update RhesisSettings.xaml.cs

--- ExtraIsland/Components/RhesisSettings.xaml.cs
+++ ExtraIsland/Components/RhesisSettings.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
+using System.Windows;
 using System.Windows.Data;
 using System.Windows.Input;
 using ExtraIsland.Shared;
+using Microsoft.Win32;
 
 namespace ExtraIsland.Components;
 
@@ -12,7 +14,8 @@ public partial class RhesisSettings {
 
     [GeneratedRegex("[^0-9]+")]
     private static partial Regex NumberRegex();
-    void TextBoxNumberCheck(object sender,TextCompositionEventArgs e) {
+    
+    void TextBoxNumberCheck(object sender, TextCompositionEventArgs e) {
         Regex re = NumberRegex();
         e.Handled = re.IsMatch(e.Text) & e.Text.Length != 0;
     }
@@ -22,12 +25,23 @@ public partial class RhesisSettings {
         RhesisDataSource.Hitokoto,
         RhesisDataSource.Jinrishici,
         RhesisDataSource.Saint,
-        RhesisDataSource.SaintJinrishici
+        RhesisDataSource.SaintJinrishici,
+        RhesisDataSource.LocalFile
     ];
+
+    void BrowseFile_Click(object sender, RoutedEventArgs e) {
+        var dialog = new OpenFileDialog {
+            Filter = "文本文件|*.txt|所有文件|*.*",
+            InitialDirectory = Path.GetFullPath(Environment.CurrentDirectory)
+        };
+        if (dialog.ShowDialog() == true) {
+            Settings.LocalFilePath = dialog.FileName;
+        }
+    }
 }
 
 public class LimitIntToArgConverter : IValueConverter {
-    public object Convert(object? value,Type targetType,object? parameter,
+    public object Convert(object? value, Type targetType, object? parameter,
         System.Globalization.CultureInfo culture) {
         return (int)value! switch {
             0 => "",
@@ -35,9 +49,8 @@ public class LimitIntToArgConverter : IValueConverter {
         };
     }
 
-    public object ConvertBack(object? value,Type targetType,object? parameter,
+    public object ConvertBack(object? value, Type targetType, object? parameter,
         System.Globalization.CultureInfo culture) {
         return 0;
     }
 }
-
